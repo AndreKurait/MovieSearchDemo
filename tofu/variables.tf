5dc72a059c4a74dd0ce4b8c4f5b0129771ffc734
Andre Kurait <akurait@amazon.com>
2026-02-17 19:52:40 -0600

    Scale ES to 6 data nodes, update storage to 2TB
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- tofu/variables.tf
+++ tofu/variables.tf
@@ -32,7 +32,7 @@ variable "es_version" {
 variable "es_replicas" {
   description = "Number of Elasticsearch StatefulSet replicas"
   type        = number
-  default     = 3
+  default     = 6
 }
 
 variable "es_storage_size" {
